3cd3cd434e5f5efb896325402b4ef0242659056a
Roy Straub <roy.straub@codesquad.nl>
2021-11-21 13:50:42 +0100

    Test descriptions

--- src/test/scala/nl/codecraftr/scala/mastermind/MastermindSpec.scala
+++ src/test/scala/nl/codecraftr/scala/mastermind/MastermindSpec.scala
@@ -7,23 +7,28 @@ class MastermindSpec extends AnyFlatSpec {
   private val allBlue = Mastermind(Code(BLUE, BLUE, BLUE, BLUE))
   private val aBlue = Mastermind(Code(BLUE, RED, RED, RED))
 
-  "four wrong pegs" should "return zero right, zero misplaced" in {
+  "four wrong pegs" should "count as zero correct, zero misplaced" in {
     val result = allBlue evaluate Code(RED, RED, RED, RED)
     assert(result == Result(0, 0))
   }
 
-  "four correct pegs" should "return four right, zero misplaced" in {
+  "four correct pegs" should "count as four correct, zero misplaced" in {
     val result = allBlue evaluate Code(BLUE, BLUE, BLUE, BLUE)
     assert(result == Result(4, 0))
   }
 
-  "a correct peg" should "return one correct, zero misplaced" in {
+  "a correct peg" should "count as one correct peg" in {
     val result = allBlue evaluate Code(BLUE, RED, RED, RED)
     assert(result == Result(1, 0))
   }
 
-  "a misplaced peg" should "return zero correct, one misplaced" in {
+  "a misplaced peg" should "count as one misplaced peg" in {
     val result = aBlue evaluate Code(YELLOW, YELLOW, YELLOW, BLUE)
     assert(result == Result(0, 1))
+  }
+
+  "a correct peg" should "have precedence over misplaced pegs" in {
+    val result = aBlue evaluate Code(BLUE, YELLOW, YELLOW, BLUE)
+    assert(result == Result(1, 0))
   }
 }
